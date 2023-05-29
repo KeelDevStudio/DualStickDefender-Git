99bc5d6977b47171c816749ba98f18bdc13b9176
KeelDevStudio <128969377+KeelDevStudio@users.noreply.github.com>
2023-05-29 22:13:45 +0200

    change animation to 2 way, anime knight, crookie 16x24

--- Actors/Characters/Player/Player.gd
+++ Actors/Characters/Player/Player.gd
@@ -33,16 +33,23 @@ var dir_move = Vector2.ZERO
 func _ready():
 	var __ = stats.connect("current_health_0", self, "_on_current_health_0")
 	state_machine.set_to_default_state()
-	$HitboxsPivot/AttacktHitBox.damage = stats.damage
+	$Pivot/PivotWeapon/AttacktHitBox.damage = stats.damage
 #	HP_bar.set_max(stats.max_health)
 #	HP_bar.set_value(stats.current_health)
 
 func _physics_process(_delta):
+	var mouse_position = get_global_mouse_position()
 	_updated_animation()
 	dir_move = Vector2.ZERO
 	dir_move.x = int(controler.right) - int(controler.left)
 	dir_move.y = int(controler.down) - int(controler.up)
 	dir_move = dir_move.normalized()
+	$Pivot/PivotWeapon.look_at(mouse_position)
+#	if dir_move.x != 0:
+	if (mouse_position.x - self.position.x) > 1:
+		$Pivot.scale.x = +1
+	elif (mouse_position.x - self.position.x) < 1: 
+		$Pivot.scale.x = -1
 
 
 ### LOGIC ###
@@ -52,10 +59,10 @@ func hurt(_damage):
 	
 func _updated_animation():
 	animation_state.travel(state_machine.get_state_name())
-	if velocity != Vector2.ZERO and not state_machine.get_state_name() == "ATTACK":
-		animation_tree.set("parameters/" + state_machine.get_state_name() + "/blend_position", velocity)
-		animation_tree.set("parameters/ATTACK/blend_position", velocity)
-	
+#	if velocity != Vector2.ZERO and not state_machine.get_state_name() == "ATTACK":
+#		animation_tree.set("parameters/" + state_machine.get_state_name() + "/blend_position", velocity)
+#		animation_tree.set("parameters/ATTACK/blend_position", velocity)
+#
 		
 
 
